74ad8e19ae49223dd2ac15c694452890e445544b
joangavelan <joangavelan@gmail.com>
2024-09-07 18:17:05 -0500

    rewrite to use a function

--- 01/farh-cel-table.c
+++ 01/farh-cel-table.c
@@ -4,8 +4,7 @@
 #define UPPER 300 /* upper limit */
 #define STEP 20   /* step size */
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300; floating-point version */
+float fahrToCel(float fahr);
 
 int main()
 {
@@ -13,45 +12,11 @@ int main()
 
   for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
   {
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+    printf("%3d %6.1f\n", fahr, fahrToCel(fahr));
   }
-
-  // float fahr, celsius;
-  // int lower, upper, step;
-
-  // lower = 0;   /* lower limit of temperature table*/
-  // upper = 300; /* upper limit */
-  // step = 20;   /* step size */
-
-  // fahr = lower;
-
-  // printf("%3s %6s\n", "Fahr", "Celsius");
-
-  // while (fahr <= upper)
-  // {
-  //   celsius = (5.0 / 9.0) * (fahr - 32.0);
-  //   printf("%3.0f %6.1f\n", fahr, celsius);
-  //   fahr = fahr + step;
-  // }
 }
 
-/* print Fahrenheit-Celcius table
-for fahr = 0, 20, ..., 300 */
-// int main()
-// {
-//   int fahr, celsius;
-//   int lower, upper, step;
-
-//   lower = 0;   /* lower limit of temperature table */
-//   upper = 300; /* upper limit */
-//   step = 20;   /* step size */
-
-//   fahr = lower;
-
-//   while (fahr <= upper)
-//   {
-//     celsius = 5 * (fahr - 32) / 9;
-//     printf("%3d\t%6d\n", fahr, celsius);
-//     fahr = fahr + step;
-//   }
-// }
+float fahrToCel(float fahr)
+{
+  return (5.0 / 9.0) * (fahr - 32.0);
+}
